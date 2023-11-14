790898dfb2cbb8c75b496e86c1f4ad3d2618540c
Deepak Kumar <deepak.kumar852182@gmail.com>
2023-11-14 22:44:39 +0530

    Remove template redirect

--- pmpro-paytm-gateway.php
+++ pmpro-paytm-gateway.php
@@ -15,20 +15,3 @@ define( 'PMPRO_PAYTMGATEWAY_URL', untrailingslashit( plugin_dir_url( __FILE__ ) 
 
 // load payment gateway class.
 require_once PMPRO_PAYTMGATEWAY_PATH . '/classes/class-pmprogateway-paytm.php';
-
-if ( ! function_exists( 'pmpro_restrict_content' ) ) {
-	/**
-	 * Restrict Content and redirect.
-	 *
-	 * @return void
-	 */
-	function pmpro_restrict_content() {
-		$has_access = pmpro_has_membership_access( null, null, false );
-		if ( ! $has_access ) {
-			wp_safe_redirect( pmpro_url( 'levels' ) );
-			exit;
-		}
-	}
-}
-
-add_action( 'template_redirect', 'pmpro_restrict_content', 1 );
